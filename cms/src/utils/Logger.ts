9453e5911ef1683e5ada411e1d863d4a3667a511
Will <williamlytle@Williams-MacBook-Pro.local>
2025-03-24 12:52:48 -0700

    add Logger util

---
+++ cms/src/utils/Logger.ts
@@ -0,0 +1,58 @@
+/**
+ * Logger class for logging messages to the GCP via stdout.
+ */
+export default class Logger {
+
+  /**
+   * Function for logging INFO messages.
+   * @param message Human Readable message
+   * @param data JSON blob of data to be logged under jsonPayload
+   */
+  public static info(message: string, data: any): void {
+    Logger.parseMessage(message, data)
+    Logger.log(console.log, data);
+  }
+
+  /**
+   * Function for logging WARN messages.
+   * @param message Human Readable message
+   * @param data JSON blob of data to be logged under jsonPayload
+   */
+  public static warn(message: string, data: any): void {
+    Logger.parseMessage(message, data)
+    Logger.log(console.warn, data);
+  }
+
+  /**
+   * Function for logging ERROR messages.
+   * @param message Human Readable message
+   * @param data JSON blob of data to be logged under jsonPayload
+   */
+  public static error(message: string, data: any): void {
+    Logger.parseMessage(message, data)
+    Logger.log(console.error, data);
+  }
+
+  /**
+   * Function to write parsed log
+   * @param loggerFn Function to be used to log the data 
+   * @param data Proccesed JSON blob of data to be logged under jsonPayload
+   */
+  private static log(loggerFn: Function, data: any): void {
+    loggerFn(JSON.stringify({ data }));
+  }
+
+  /**
+   * Helper function to parse message and data into a single string
+   * @param message Human readable message
+   * @param data JSON blob of data to be logged under jsonPayload
+   * @returns 
+   */
+  private static parseMessage(message: string, data: any): void {
+    if (data.message) {
+      data.message = message + ": " + data.message;
+    } else {
+      data.message = message;
+    }
+  }
+}
