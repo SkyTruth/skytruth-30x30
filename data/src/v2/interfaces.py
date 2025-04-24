e8332f20e71a842f9bd2e6de441f23e5587ba5a4
Will Lytle <wlytle89@gmail.com>
2025-04-24 11:56:03 -0700

    Revert "update data pipelines"

--- data/src/v2/interfaces.py
+++
@@ -1,7 +0,0 @@
-from typing import Tuple
-
-BoundingBox = Tuple[float, float, float, float]
-"""A ``(west, south, east, north)`` tuple."""
-
-WindowWH = Tuple[int, int]
-"""A ``(with, heigh)`` tuple."""
