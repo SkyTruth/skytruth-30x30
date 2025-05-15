a909949c8c2b42ef359c41d972ff2dd7ea71a807
Will <williamlytle@Williams-MacBook-Pro.local>
2025-05-14 17:46:21 -0700

    add tests

--- cloud_functions/data_processing/src/utils/logger.py
+++ cloud_functions/data_processing/src/utils/logger.py
@@ -11,41 +11,19 @@ class Logger:
         self.project = os.environ.get("PROJECT")
         self.request = request
 
-    def _format_structured_payload(self, payload: dict) -> dict:
-        """Format the payload to be sent to GCP logging service"""
-        global_log_fields = {}
-
-        # Add log correlation to nest all log messages.
-        # This is only relevant in HTTP-based contexts, and is ignored elsewhere.
-        # (In particular, non-HTTP-based Cloud Functions.)
-        if self.request:
-            trace_header = self.request.headers.get("X-Cloud-Trace-Context")
-
-            if trace_header and self.project:
-                trace = trace_header.split("/")
-                global_log_fields["logging.googleapis.com/trace"] = (
-                    f"projects/{self.project}/traces/{trace[0]}"
-                )
-
-        return dict(
-            **payload,
-            **global_log_fields,
-        )
 
     def info(self, payload: dict) -> None:
         """Log an info message to GCP logging service"""
-        payload = self._format_structured_payload(payload)
         payload["severity"] = "INFO"
         self.log(payload)
 
     def warning(self, payload: dict) -> None:
         """Log a warning message to GCP logging service"""
-        payload = self._format_structured_payload(payload)
         payload["severity"] = "WARNING"
+        self.log(payload)
 
     def error(self, payload: dict) -> None:
         """Log an error message to GCP logging service"""
-        payload = self._format_structured_payload(payload)
         payload["severity"] = "ERROR"
         self.log(payload)
 
