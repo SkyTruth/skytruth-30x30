8afd682a123426c212a931824e70f1e715a676ee
Will <williamlytle@Williams-MacBook-Pro.local>
2025-05-22 13:56:50 -0700

    resolve merge conflicts

--- cms/src/api/pa/routes/pa.ts
+++ cms/src/api/pa/routes/pa.ts
@@ -5,10 +5,6 @@
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreRouter('api::pa.pa', {
-<<<<<<< HEAD
-  only: ['find', 'findOne', 'update', 'create',]
-=======
   only: ['find', 'findOne', 'create', 'update'],
->>>>>>> feature/api-data-integration
 });
 
