fd5cf61c91ef4df1623354ece2e763f553062b87
Will <williamlytle@Mac.MG8702>
2025-07-08 15:36:11 -0700

    add try catch logic to feature flag controller

--- cms/src/api/feature-flag/controllers/feature-flag.ts
+++ cms/src/api/feature-flag/controllers/feature-flag.ts
@@ -7,6 +7,7 @@ import { factories } from '@strapi/strapi'
 export default factories.createCoreController('api::feature-flag.feature-flag',
   ({ strapi }) => ({
     async find(ctx) {
+      try {
       const { query, request: { header } } = ctx
 
       // User defined runAsOf date preferentailly uses the header, then query, then defaults to now
@@ -22,6 +23,10 @@ export default factories.createCoreController('api::feature-flag.feature-flag',
         .validateFeatureFlags(data.data, runAsOf);
 
       return ctx.send(validatedFeatureFlags)
+    } catch (error) {
+        strapi.log.error('Error in feature-flag controller find:', error);
+        return ctx.badRequest('Failed to retrieve feature flags', { error: error.message });
+    }
     }
   })
 );
