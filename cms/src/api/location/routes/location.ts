7328c29b90f73826e9053293e89892e6f8d80967
Will <williamlytle@Williams-MacBook-Pro.local>
2025-07-10 15:55:26 -0700

    add autehnticated write permissions for lcoations

--- cms/src/api/location/routes/location.ts
+++ cms/src/api/location/routes/location.ts
@@ -5,5 +5,5 @@
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreRouter('api::location.location', {
-    only: ['find', 'findOne', 'update']
+    only: ['find', 'findOne', 'update', 'create']
 });
