6ea1a90c84cf98d3d97b172a489238258cf630d3
Will <williamlytle@Williams-MacBook-Pro.local>
2025-08-19 12:09:10 -0700

    install tippecanoe and init tippiecanoe util

---
+++ cloud_functions/data_processing/src/core/tippecanoe.py
@@ -0,0 +1,61 @@
+# import subprocess
+
+# def json2mbtiles(
+#     source_path: Path, output_path: Union[Path, None] = None, **kwargs: ExcessParams
+# ) -> Path:
+#     if not output_path:
+#         output_path = source_path.with_suffix(".json")
+#     # https://github.com/mapbox/tippecanoe
+#     # -zg: Automatically choose a maxzoom that should be sufficient to clearly distinguish the
+#     #   features and the detail within each feature
+#     # -f: Deletes existing files in location with the same name
+#     # -P: reads the geojson in parallel if the file is newline delimited
+#     # -o: The following argurment is hte output path
+#     # -ae:  Increase the maxzoom if features are still being dropped at that zoom level
+#     subprocess.run(
+#         f"tippecanoe -zg -f -P -o {output_path} -ae {source_path}",
+#         shell=True,
+#         check=True,
+#     )
+#     source_path.unlink()
+#     return output_path
+
+
+# def mbtileGeneration(
+#     data_path: Path,
+#     output_path: Union[Path, None] = None,
+#     update: bool = False,
+#     **kwargs: ExcessParams,
+# ) -> Path:
+#     """
+#     generate mbtiles file from geomtry file
+
+#     Args:
+#         data_path (Path): The path to the file.
+#         output_path (Union[Path, None], optional): The path to the output file. The default is None.
+#         update (bool, optional): Whether to update the mbtiles file. The default is False.
+
+#     Returns:
+#         Path: The path to the output file.
+
+#     """
+#     try:
+#         if not data_path.exists():
+#             raise FileNotFoundError("Data path does not exist.")
+
+#         if not output_path:
+#             output_path = data_path.with_suffix(".mbtiles")
+
+#         if update or not output_path.exists():
+#             if data_path.suffix != ".json":
+#                 data_path = simplifyGeometries2Json(data_path, **kwargs)
+#             if data_path.suffix != ".json":
+#                 raise Exception("Data path must be a json file.")
+
+#             logging.info("Creating mbtiles file...")
+#             json2mbtiles(data_path, output_path, **kwargs)
+
+#         return output_path
+
+#     except Exception as e:
+#         raise e
