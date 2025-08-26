d418ba9cfd976e94b1b083fb9860fe4c2688ef86
Will <williamlytle@Williams-MacBook-Pro.local>
2025-08-26 13:32:21 -0700

    linting

--- frontend/src/containers/map/sidebar/main-panel/location-selector/location-dropdown/index.tsx
+++ frontend/src/containers/map/sidebar/main-panel/location-selector/location-dropdown/index.tsx
@@ -27,7 +27,7 @@ enum LocationType {
   region = 'region',
   highseas = 'highseas',
   worldwide = 'worldwide',
-};
+}
 
 const LocationDropdown: FCWithMessages<LocationDropdownProps> = ({
   className,
@@ -69,7 +69,7 @@ const LocationDropdown: FCWithMessages<LocationDropdownProps> = ({
       <CommandEmpty>{t('no-result')}</CommandEmpty>
       <CommandGroup className="mt-4 max-h-64 overflow-y-auto">
         {locations.map(({ attributes }) => {
-          const { name, name_es, name_fr, code, type} = attributes;
+          const { name, name_es, name_fr, code, type } = attributes;
 
           let locationName = name;
           if (locale === 'es') {
