7e26bfb9b87aac912230f62f3331cc0736c814ed
Will <williamlytle@Williams-MacBook-Pro.local>
2025-10-09 16:31:34 -0700

    hide pt behind feature flag

--- frontend/src/components/header/dropdowns/language-selector/index.tsx
+++ frontend/src/components/header/dropdowns/language-selector/index.tsx
@@ -3,11 +3,13 @@ import { useRouter } from 'next/router';
 import { useLocale, useTranslations } from 'next-intl';
 
 import { Select, SelectTrigger, SelectContent, SelectItem } from '@/components/ui/select';
+import { useFeatureFlag } from '@/hooks/use-feature-flag';
 import { FCWithMessages } from '@/types';
 
 const LanguageSelector: FCWithMessages = () => {
   const t = useTranslations('components.language-selector');
   const locale = useLocale();
+  const isPTActive = useFeatureFlag('is_pt_active');
 
   const { push, asPath, pathname, query } = useRouter();
 
@@ -19,7 +21,14 @@ const LanguageSelector: FCWithMessages = () => {
       <SelectTrigger variant="alternative">
         <span className="sr-only">
           {t('selected-language', {
-            language: locale === 'es' ? t('spanish') : locale === 'fr' ? t('french') : t('english'),
+            language:
+              locale === 'es'
+                ? t('spanish')
+                : locale === 'fr'
+                  ? t('french')
+                  : locale === 'pt'
+                    ? t('portuguese')
+                    : t('english'),
           })}
         </span>
         <span className="not-sr-only">{locale.toLocaleUpperCase()}</span>
@@ -28,7 +37,9 @@ const LanguageSelector: FCWithMessages = () => {
         <SelectItem value="en">English{locale !== 'en' && ` (${t('english')})`}</SelectItem>
         <SelectItem value="es">Español{locale !== 'es' && ` (${t('spanish')})`}</SelectItem>
         <SelectItem value="fr">Français{locale !== 'fr' && ` (${t('french')})`}</SelectItem>
-        <SelectItem value="pt">Português{locale !== 'pt' && ` (${t('portuguese')})`}</SelectItem>
+        {isPTActive ? (
+          <SelectItem value="pt">Português{locale !== 'pt' && ` (${t('portuguese')})`}</SelectItem>
+        ) : null}
       </SelectContent>
     </Select>
   );
