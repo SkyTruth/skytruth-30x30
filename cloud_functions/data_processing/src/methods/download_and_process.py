247dc38ce783963b5eec47b4b5cc9058ab5cacdb
Susan Stillman <suzy.stillman@gmail.com>
2025-11-13 13:57:14 -0700

    logger typo...sigh...

--- cloud_functions/data_processing/src/methods/download_and_process.py
+++ cloud_functions/data_processing/src/methods/download_and_process.py
@@ -340,9 +340,9 @@ def download_and_process_protected_planet_pas(
                 parquet_file = pq.ParquetFile(p)
                 total_rows = parquet_file.metadata.num_rows
                 est_batches = int(np.ceil(total_rows / batch_size))
-                logger.info( {
-                    "message": f"Processing {p}: {total_rows} rows, ~{est_batches} batches"
-                })
+                logger.info(
+                    {"message": f"Processing {p}: {total_rows} rows, ~{est_batches} batches"}
+                )
 
                 results = Parallel(n_jobs=n_jobs, backend="loky", timeout=600)(
                     delayed(simplify_chunk)(
