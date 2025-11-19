aa86cbcf2a485dca23c6feb272c3df68620ac881
Susan Stillman <suzy.stillman@gmail.com>
2025-11-19 12:29:22 -0700

    more triggers, add check for PA updates before triggering tileset updates

--- cloud_functions/data_processing/src/methods/generate_tables.py
+++ cloud_functions/data_processing/src/methods/generate_tables.py
@@ -142,6 +142,7 @@ def generate_protected_areas_diff_table(
     # return True if the database is being updated, otherwise False
     return len(db_changes["new"]) + len(db_changes["changed"]) > 0
 
+
 def dissolve_multipolygons(gdf: gpd.GeoDataFrame, key: str = "WDPAID") -> gpd.GeoDataFrame:
     counts = gdf[key].value_counts()
 
