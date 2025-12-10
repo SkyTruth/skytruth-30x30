aa20db35eaf26c7141c9e62a5dd86ef7d0d21f0c
Olivia <144152732+via-zhang@users.noreply.github.com>
2025-12-10 13:33:14 -0500

    add method to dissolve geometries

--- cloud_functions/data_processing/src/methods/subtract_geometries.py
+++ cloud_functions/data_processing/src/methods/subtract_geometries.py
@@ -4,37 +4,100 @@ from joblib import Parallel, delayed
 from tqdm.auto import tqdm
 from src.utils.gcp import (
     read_json_df,       # Reads a .json or .geojson file from GCS and returns a DataFrame or GeoDataFrame
-    upload_gdf_zip      # Saves a GeoDataFrame to GCS as a zipped file
+    upload_gdf_zip,     # Saves a GeoDataFrame to GCS as a zipped file
+    upload_gdf          # Saves a GeoDataFrame to GCS as a .geojson file
 )
 from src.utils.logger import Logger
 
 logger = Logger()
 
-def process_country(data_tuple: tuple):
+def process_country(country_area: gpd.GeoDataFrame, 
+                    country_pa: gpd.GeoDataFrame):
     """
     Subtracts protected areas from total area for a country.
 
     Parameters
     ----------
-    data_tuple : tuple
-        A tuple containing (country_area, country_pa).
+    country_pa : gpd.GeoDataFrame
+        GeoDataFrame with protected areas for a country.
+    country_area : gpd.GeoDataFrame
+        GeoDataFrame with total area for a country.
 
     Returns
     -------
         GeoDataFrame with protected areas subtracted from total area for a country.
     """
-    country_area, country_pa = data_tuple
-    if country_pa is None:
+    if country_pa.empty:
         return country_area
     country_area = country_area.copy()
-    country_area['geometry'] = country_area.geometry.difference(country_pa)
+    country_area['geometry'] = country_area.difference(country_pa)
     return country_area
 
-def generate_total_area_minus_pa(bucket: str, 
-                                 total_area_file: str, 
-                                 pa_file: str, 
-                                 out_file: str, 
-                                 tolerance: float, 
+def dissolve_geometries(bucket: str,
+                        gdf_file: str,
+                        out_file: str,
+                        tolerance: float,
+                        verbose: bool = True
+    ):
+    """
+    Loads a GeoDataFrame from GCS, dissolves geometries by country,
+    and saves the output as a .geojson to GCS.
+
+    Parameters
+    ----------
+    bucket : str
+        GCS bucket name.
+    gdf_file : str
+        Filename of GeoDataFrame GeoJSON in GCS.
+    out_file : str
+        Filename for output GeoJSON file in GCS.
+    tolerance : float
+        Tolerance value used in simplification.
+    verbose : bool, optional
+        Whether to print verbose logs, by default True.
+    
+    Returns
+    -------
+        GeoDataFrame saved to GCS as a GeoJSON file.
+    """
+
+    # Load GeoDataFrame from GCS
+    pa = read_json_df(
+        bucket_name=bucket,
+        filename=gdf_file.replace('.geojson', f'_{tolerance}.geojson'),
+        verbose=verbose
+    )
+
+    # Keep only polygon records and make the geometries valid
+    pa = pa[pa.geometry.geom_type.isin(['MultiPolygon', 'Polygon'])]
+    pa.geometry = pa.geometry.make_valid()
+
+    # Label Antarctica PAs as ABNJ (areas beyond national jurisdiction)
+    pa.loc[pa["ISO3"] == "ATA", "ISO3"] = "ABNJ"
+
+    # Dissolve geometries by unique ISO3 codes
+    if verbose:
+        print(f'Dissolving geometries by ISO3 codes...')
+    pa["location"] = pa['ISO3'].str.split(';')
+    pa = pa.explode("location", ignore_index=True)
+    dissolved = pa[['location', 'geometry']].dissolve(by='location').reset_index()
+
+    if verbose:
+        print(f'Output file has {len(dissolved)} rows.')
+
+    # Save to GCS as zipped shapefile
+    upload_gdf(
+        bucket_name=bucket,
+        gdf=dissolved,
+        destination_blob_name=out_file.replace('.geojson', f'_{tolerance}.geojson')
+    )
+
+def generate_total_area_minus_pa(bucket: str,
+                                 total_area_file: str,
+                                 pa_file: str,
+                                 is_processed: bool,
+                                 out_file: str,
+                                 tolerance: float,
                                  verbose: bool = True
     ):
     """
@@ -49,6 +112,8 @@ def generate_total_area_minus_pa(bucket: str,
         Filename of total area geojson (GADM or EEZ).
     pa_file : str
         Filename of protected area geojson (PA or MPA).
+    is_processed : bool
+        Whether the protected areas GeoDataFrame is already processed and dissolved by country.
     out_file : str
         Filename for output zipped file.
     tolerance : float
@@ -69,35 +134,40 @@ def generate_total_area_minus_pa(bucket: str,
     )
     total_area = total_area[['location', 'geometry']]
 
-    # Protected areas: PA (terrestrial) or MPA (marine)
+    # Dissolved protected areas: PA (terrestrial) or MPA (marine)
     pa = read_json_df(
         bucket_name=bucket,
         filename=pa_file.replace('.geojson', f'_{tolerance}.geojson'),
         verbose=verbose
     )
 
-    # Keep only polygon records and make the geometries valid
-    pa = pa[pa.geometry.geom_type.isin(['MultiPolygon', 'Polygon'])]
-    pa.geometry = pa.geometry.make_valid()
-
-    # Label Antarctica PAs as ABNJ (areas beyond national jurisdiction)
-    pa.loc[pa["ISO3"] == "ATA", "ISO3"] = "ABNJ"
-
-    # Create country-specific subsets
+    # Get list of unique country codes
     countries = total_area['location'].unique().tolist()
-    if verbose:
-        print(f'Dissolving {len(countries)} country geometries...')
-    country_data = [
-        (total_area[total_area['location'] == country],
-        pa[pa['ISO3'] == country].union_all() if not pa[pa['ISO3'] == country].empty else None)
-        for country in countries
-    ]
 
-    # Subtract PAs from total areas in parallel
+    if not is_processed:
+        # Keep only polygon records and make the geometries valid
+        pa = pa[pa.geometry.geom_type.isin(['MultiPolygon', 'Polygon'])]
+        pa.geometry = pa.geometry.make_valid()
+
+        # Label Antarctica PAs as ABNJ (areas beyond national jurisdiction)
+        pa.loc[pa["ISO3"] == "ATA", "ISO3"] = "ABNJ"
+
+        # Dissolve country geometries by location
+        if verbose:
+            print(f'Dissolving {len(countries)} country geometries...')
+        pa["location"] = pa['ISO3'].str.split(';')
+        pa = pa.explode("location", ignore_index=True)
+        pa = pa[['location', 'geometry']].dissolve(by='location').reset_index()
+    
+    # Subtract geometries
     if verbose:
         print(f'Subtracting protected areas from total areas...')
-    results = Parallel(n_jobs=-1)(
-        delayed(process_country)(data) for data in tqdm(country_data)
+    results = Parallel(n_jobs=-1, backend='loky')(
+        delayed(process_country)(
+            total_area[total_area['location'] == country].reset_index(),
+            pa[pa['location'] == country].reset_index()
+        )
+        for country in tqdm(countries)
     )
 
     total_area_minus_pa = pd.concat(results).reset_index(drop=True)
