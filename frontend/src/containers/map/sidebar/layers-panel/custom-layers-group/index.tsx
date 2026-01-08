5e04142e5b2ce726887baa54f5e64d896badd913
Will <williamlytle@Williams-MacBook-Pro.local>
2026-01-08 11:25:35 -0700

    add custom layer group component

---
+++ frontend/src/containers/map/sidebar/layers-panel/custom-layers-group/index.tsx
@@ -0,0 +1,156 @@
+import { PropsWithChildren, useCallback, useEffect, useState } from 'react';
+
+import { useTranslations } from 'next-intl';
+import { LuChevronDown, LuChevronUp } from 'react-icons/lu';
+
+import TooltipButton from '@/components/tooltip-button';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
+import { Label } from '@/components/ui/label';
+import { Switch } from '@/components/ui/switch';
+import { useSyncMapLayers } from '@/containers/map/content/map/sync-settings';
+import { useSyncMapContentSettings } from '@/containers/map/sync-settings';
+import { cn } from '@/lib/classnames';
+import { FCWithMessages } from '@/types';
+import { Layer } from '@/types/generated/strapi.schemas';
+import { UserLayer } from '@/types/layers';
+
+import UploadLayer from '../../main-panel/panels/details/upload-layer';
+
+export const SWITCH_LABEL_CLASSES = '-mb-px cursor-pointer pt-px font-mono text-xs font-normal';
+const COLLAPSIBLE_TRIGGER_ICONS_CLASSES = 'w-5 h-5 hidden';
+const COLLAPSIBLE_TRIGGER_CLASSES =
+  'group flex w-full items-center justify-between py-2 text-xs font-bold';
+const COLLAPSIBLE_CONTENT_CLASSES =
+  'data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down border-black';
+
+type CustomLayersGroupProps = PropsWithChildren<{
+  name: string;
+  layers: UserLayer[];
+  showDatasetsNames?: boolean;
+  showBottomBorder?: boolean;
+  isOpen?: boolean;
+  loading?: boolean;
+  // Number of extra active layers for this group
+  extraActiveLayers?: number;
+}>;
+
+const CustomLayersGroup: FCWithMessages<CustomLayersGroupProps> = ({
+  name,
+  layers,
+  showDatasetsNames = true,
+  showBottomBorder = true,
+  isOpen = true,
+  loading = true,
+  children,
+}): JSX.Element => {
+  const [open, setOpen] = useState(isOpen);
+  const t = useTranslations('containers.map-sidebar-layers-panel');
+
+  const [activeLayers, setMapLayers] = useSyncMapLayers();
+  const [{ tab }] = useSyncMapContentSettings();
+
+  // const datasetsLayersIds = useMemo(() => {
+  //   return (
+  //     datasets?.map(({ attributes }) => attributes?.layers?.data?.map(({ id }) => id))?.flat() || []
+  //   );
+  // }, [datasets]);
+
+  // const numActiveDatasetsLayers = useMemo(() => {
+  //   return (
+  //     (datasetsLayersIds?.filter((id) => activeLayers?.includes(id))?.length ?? 0) +
+  //     extraActiveLayers
+  //   );
+  // }, [datasetsLayersIds, activeLayers, extraActiveLayers]);
+
+  const onToggleLayer = useCallback(
+    (layerSlug: Layer['slug'], isActive: boolean) => {
+      setMapLayers(
+        isActive
+          ? [...activeLayers, layerSlug]
+          : activeLayers.filter((activeSlug) => activeSlug !== layerSlug)
+      );
+    },
+    [activeLayers, setMapLayers]
+  );
+
+  useEffect(() => {
+    setOpen(isOpen);
+  }, [isOpen, tab]);
+
+  // const displayNumActiveLayers = !open && numActiveDatasetsLayers > 0;
+  // const noData = !loading && !layers?.length;
+
+  return (
+    <Collapsible open={open} onOpenChange={setOpen}>
+      <CollapsibleTrigger
+        className={cn(COLLAPSIBLE_TRIGGER_CLASSES, { 'border-b border-black': !open })}
+      >
+        <span>
+          {name}
+          {/* {displayNumActiveLayers && (
+            <span className="ml-2 border border-black px-1 font-normal">
+              {numActiveDatasetsLayers}
+            </span>
+          )} */}
+        </span>
+        <LuChevronDown
+          className={`group-data-[state=closed]:block ${COLLAPSIBLE_TRIGGER_ICONS_CLASSES}`}
+        />
+        <LuChevronUp
+          className={`group-data-[state=open]:block ${COLLAPSIBLE_TRIGGER_ICONS_CLASSES}`}
+        />
+      </CollapsibleTrigger>
+      <CollapsibleContent
+        className={cn(COLLAPSIBLE_CONTENT_CLASSES, { 'border-b': showBottomBorder })}
+      >
+        <div>
+          {loading && <span className="font-mono text-xs">{t('loading')}</span>}
+          {/* {noData && <span className="font-mono text-xs">{t('no-data-available')}</span>} */}
+          <UploadLayer />
+          <div>
+            <ul className={cn('my-3 flex flex-col space-y-3', { '-my-0': !showDatasetsNames })}>
+              {layers.map((layer) => {
+                const isActive = activeLayers?.includes(layer?.name);
+                const onCheckedChange = onToggleLayer.bind(null, layer?.name) as (
+                  isActive: boolean
+                ) => void;
+                // const metadata = layer?.attributes?.metadata;
+                // const sources = metadata?.citation
+                //   ? [{ id: layer?.id, title: metadata?.citation, url: metadata?.source }]
+                //   : null;
+
+                return (
+                  <li key={layer.name} className="flex items-start justify-between">
+                    <span className="flex items-start gap-2">
+                      <Switch
+                        id={`${layer.name}-switch`}
+                        className="mt-px"
+                        checked={isActive}
+                        onCheckedChange={onCheckedChange}
+                      />
+                      <Label htmlFor={`${layer.name}-switch`} className={SWITCH_LABEL_CLASSES}>
+                        {layer.name}
+                      </Label>
+                    </span>
+                    {/* {metadata?.description && (
+                          <TooltipButton
+                            className="mt-px"
+                            text={metadata?.description}
+                            sources={sources}
+                          />
+                        )} */}
+                  </li>
+                );
+              })}
+              <>{children}</>
+            </ul>
+          </div>
+        </div>
+      </CollapsibleContent>
+    </Collapsible>
+  );
+};
+
+CustomLayersGroup.messages = ['containers.map-sidebar-layers-panel', ...TooltipButton.messages];
+
+export default CustomLayersGroup;
