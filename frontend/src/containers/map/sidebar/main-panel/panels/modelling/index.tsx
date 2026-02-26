979abd08a934736f195370806d85f48d65853513
Will <williamlytle@Williams-MacBook-Pro.local>
2026-02-26 08:36:07 -0700

    fix shrinking header on scroll in modelling side bar

--- frontend/src/containers/map/sidebar/main-panel/panels/modelling/index.tsx
+++ frontend/src/containers/map/sidebar/main-panel/panels/modelling/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef } from 'react';
 
 import { useAtomValue } from 'jotai';
 import { useTranslations } from 'next-intl';
@@ -7,6 +7,8 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { modellingAtom } from '@/containers/map/store';
 import { useSyncMapContentSettings } from '@/containers/map/sync-settings';
 import useMapDefaultLayers from '@/hooks/use-map-default-layers';
+import useScrollPosition from '@/hooks/use-scroll-position';
+import { cn } from '@/lib/classnames';
 import { FCWithMessages } from '@/types';
 
 import LocationSelector from '../../location-selector';
@@ -18,10 +20,8 @@ import ModellingWidget from './widget';
 const SidebarModelling: FCWithMessages = () => {
   const t = useTranslations('containers.map-sidebar-main-panel');
 
-  const [headerSize, setHeaderSize] = useState<string | null>(null);
-
   const contentRef = useRef<HTMLDivElement | null>(null);
-  const containerRef = useRef<HTMLDivElement | null>(null);
+  const contentScroll = useScrollPosition(contentRef);
 
   const { status: modellingStatus } = useAtomValue(modellingAtom);
   const [{ tab }, setSettings] = useSyncMapContentSettings();
@@ -50,41 +50,17 @@ const SidebarModelling: FCWithMessages = () => {
     }
   }, [setSettings, tab]);
 
-  // Dynamically shrink the panel header based on available height
-  useEffect(() => {
-    const updateHeight = () => {
-      if (containerRef.current) {
-        let textSize = 'text-xl';
-        const {
-          current: { clientHeight },
-        } = containerRef;
-
-        if (clientHeight <= 700 && clientHeight > 630) {
-          textSize = 'text-3xl';
-        }
-        if (clientHeight > 700) {
-          textSize = 'text-5xl';
-        }
-        setHeaderSize(textSize);
-      }
-    };
-
-    updateHeight();
-    window.addEventListener('resize', updateHeight);
-
-    return () => window.removeEventListener('resize', updateHeight);
-  }, []);
-
   return (
-    <Tabs
-      value={tab}
-      onValueChange={handleTabChange}
-      className="flex h-full w-full flex-col"
-      ref={containerRef}
-    >
+    <Tabs value={tab} onValueChange={handleTabChange} className="flex h-full w-full flex-col">
       <div className="flex flex-shrink-0 flex-col gap-y-2 border-b border-black bg-blue-600 px-4 pt-4 md:px-8 md:pt-6">
         <div>
-          <h1 className={`text-ellipsis font-black transition-all ${headerSize}`}>
+          <h1
+            className={cn({
+              'text-ellipsis font-black transition-all': true,
+              'min-h-[3rem] text-5xl': contentScroll === 0,
+              'min-h-[1.75rem] text-xl': contentScroll > 0,
+            })}
+          >
             {showIntro ? t('conservation-scenarios') : t('custom-area')}
           </h1>
         </div>
