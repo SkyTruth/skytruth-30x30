d90122778a6162757d5492b7ebf9f4bb669942fa
sstill88 <38501385+sstill88@users.noreply.github.com>
2026-09-02 13:10:51 -0700

    Apply suggestion from @wlytle
    Co-authored-by: Will Lytle <will@skytruth.org>

--- cloud_functions/data_processing/src/methods/protected_areas/protected_areas.py
+++ cloud_functions/data_processing/src/methods/protected_areas/protected_areas.py
@@ -246,13 +246,11 @@ def generate_protected_areas_table(
 
     if verbose:
         logger.info({"message": f"loading gadm from {gadm_file_name}"})
-    gadm = read_json_df(BUCKET, gadm_file_name)
-    gadm = calculate_area(gadm, output_area_column="AREA_KM2")
+terrestrial_areas = read_json_df(BUCKET, gadm_file_name)
+terrestrial_areas = calculate_area(gadm, output_area_column="AREA_KM2")
 
     iho_areas = load_iho_regions().rename(columns={"area": "AREA_KM2"})
     marine_areas = pd.concat([eez, iho_areas[["location", "AREA_KM2"]]], ignore_index=True)
-    terrestrial_areas = gadm
-
     if verbose:
         logger.info({"message": "processing WDPAs"})
     wdpa_pa = process_wdpa(wdpa)
